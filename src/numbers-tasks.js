f149f36c760c180a8401a7941bcffd30882ace2e
Victoria Pratkina <victoria.pratkina@gmail.com>
2025-10-26 20:55:46 +0100

    feat: 10 tasks done

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -520,8 +520,8 @@ function roundToNearestInteger(/* number */) {
  * 5.4  => 5
  * -5.5 => -5
  */
-function getIntegerPartNumber(/* number */) {
-  throw new Error('Not implemented');
+function getIntegerPartNumber(number) {
+  return Math.trunc(number);
 }
 
 /**
@@ -536,8 +536,10 @@ function getIntegerPartNumber(/* number */) {
  * 1, 2, 3       => 6
  * 0.1, 0.2, 0.3 => 0.6
  */
-function getSumOfNumbers(/* x1, x2, x3 */) {
-  throw new Error('Not implemented');
+function getSumOfNumbers(x1, x2, x3) {
+  return (
+    (Math.round(x1 * 100) + Math.round(x2 * 100) + Math.round(x3 * 100)) / 100
+  );
 }
 
 /**
